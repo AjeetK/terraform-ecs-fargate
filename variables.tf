d9f5e2d38f5118e8f219b64ca9491b976cd37b87
Ajeet <ajeet.khan@experitest.com>
2020-04-05 00:19:53 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-west-2"
+}
+
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default = "myEcsTaskExecutionRole"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "nginx:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
+}
